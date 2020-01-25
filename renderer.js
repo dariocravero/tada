ed3ebea3eb5253bd173ca559e8d54ceac18de78d
Darío Javier Cravero <dario@uxtemple.com>
2020-01-25 18:48:08 +0000

    tada

--- renderer.js
+++ renderer.js
@@ -4,3 +4,26 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+//
+
+let myCanvas = document.createElement('canvas')
+myCanvas.style.width = '100vw'
+myCanvas.style.height = '100vh'
+document.body.appendChild(myCanvas)
+
+let myConfetti = window.confetti.create(myCanvas, {
+  resize: true,
+  useWorker: true,
+})
+setInterval(() => {
+  myConfetti({
+    particleCount: 100,
+    startVelocity: 30,
+    spread: 360,
+    origin: {
+      x: Math.random(),
+      // since they fall down, start a bit higher than random
+      y: Math.random() - 0.2,
+    },
+  })
+}, 2500)
